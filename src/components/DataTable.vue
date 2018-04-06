6750edff02601e329470d6cb287c946c8450965a
suits-at <office@suits.at>
2018-04-06 11:57:51 +0200

    move css into datatable component

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -39,5 +39,65 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+#container {
+  max-width: 1200px;
+}
+
+.textRight {
+  text-align: right;
+}
+
+.textCenter {
+  text-align: center;
+}
+
+.negative {
+  color: red;
+}
+
+.positive {
+  color: limegreen;
+}
+tr {
+  text-align: right;
+}
+
+td:nth-child(2),
+td:nth-child(3),
+th:nth-child(2),
+th:nth-child(3) {
+  text-align: left;
+}
+
+td:nth-child(1),
+th:nth-child(1),
+th:nth-child(6),
+th:nth-child(7),
+th:nth-child(8) {
+  text-align: center;
+}
+
+.dtr-details {
+  text-align: left;
+}
+
+.dtr-title {
+  text-align: left;
+}
+.dtr-data {
+  text-align: right;
+  padding-left: 20px;
+  float: right;
+}
+
+.dataTables_wrapper {
+  padding-top: 1.2rem;
+}
+
+/* override table padding from datatables.css */
+table.dataTable tbody th,
+table.dataTable tbody td {
+  padding: 8px 4px;
+}
 </style>
