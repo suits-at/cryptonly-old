16920500f92e30f9854f297522c9a93af4c9aac2
suits-at <office@suits.at>
2018-04-06 12:50:14 +0200

    get vue-tables-2 running

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -16,21 +16,22 @@ import { ClientTable } from "vue-tables-2";
 
 Vue.use(ClientTable);
 
-/* eslint-disable no-new */
-new Vue({
-  el: "#people",
-  data: {
-    columns: ["id", "name", "age"],
-    tableData: [
-      { id: 1, name: "John", age: "20" },
-      { id: 2, name: "Jane", age: "24" },
-      { id: 3, name: "Susan", age: "16" },
-      { id: 4, name: "Chris", age: "55" },
-      { id: 5, name: "Dan", age: "40" }
-    ],
-    options: {
-      // see the options API
-    }
+export default {
+  name: "GainersToolbar",
+  data() {
+    return {
+      columns: ["id", "name", "age"],
+      tableData: [
+        { id: 1, name: "John", age: "20" },
+        { id: 2, name: "Jane", age: "24" },
+        { id: 3, name: "Susan", age: "16" },
+        { id: 4, name: "Chris", age: "55" },
+        { id: 5, name: "Dan", age: "40" }
+      ],
+      options: {
+        // see the options API
+      }
+    };
   }
-});
+};
 </script>
